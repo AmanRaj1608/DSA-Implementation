beeb54923b5831b83ed8fa084278d8d77572181c
Aman Rojjha <53688395+SarcasticNastik@users.noreply.github.com>
2019-10-06 17:05:23 +0530

    Square Root of a Positive Number!

---
+++ Divide and Conquer/SquareRoot.c
@@ -0,0 +1,31 @@
+#inlcude<math.h>
+#include<stdio.h>
+#define PR 0.0000001
+int main(void)
+{
+  float n,ans,l,r,mid;
+  printf("Please enter a positive number!");
+  scanf("%d",&n);
+  if(n>1)
+  {
+     l =1,r=n;
+     mid = (l+r)/2;   
+     while(fabs(mid*mid-n)>PR)
+     {
+          ((mid*mid-n)>0)?r=mid:l=mid;
+     }
+      ans=mid;
+  }
+  else
+  {
+    
+     l =1,r=n;
+     mid = (l+r)/2;   
+     while(fabs(mid*mid-n)>PR)
+     {
+          ((mid*mid-n)>0)?l=mid:r=mid;
+     }
+      ans=mid;
+  }
+  return 0;
+}
